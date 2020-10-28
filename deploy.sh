1fd35a2432181c20ca3c66e8e1c74ad702a94509
chadcarlson <chad.carlson@platform.sh>
2020-10-28 13:26:16 -0400

    Another fresh start, attempt to initialize.

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,18 @@ fi
 
 # First deploy database installation
 if [ ! -f var/platformsh.installed ]; then
+
+    # Alias the CLI
+    alias directus='node node_modules/directus/dist/cli'
+
     # Install the database
-    node node_modules/directus/dist/cli database install
+    directus database install
+
+    # Create the admin user role
+    ROLE_UUID=$(directus roles create --name admin --admin)
+
+    # Pipe output of above command into this one, adding an initial admin user
+    directus users create --email admin@example.com --password password --role $ROLE_UUID
 
     # Create file that indicates first deploy and installation has been completed.
     touch var/platformsh.installed
