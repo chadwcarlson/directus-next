7e200d715df3615399c0d948625e8e60f5580797
chadcarlson <chad.carlson@platform.sh>
2020-10-28 13:31:58 -0400

    forget the alias for now.

--- deploy.sh
+++ deploy.sh
@@ -13,16 +13,19 @@ fi
 if [ ! -f var/platformsh.installed ]; then
 
     # Alias the CLI
-    alias directus='node node_modules/directus/dist/cli'
+    # alias directus='node node_modules/directus/dist/cli'
 
     # Install the database
-    directus database install
+    # directus database install
+    node node_modules/directus/dist/cli database install
 
     # Create the admin user role
-    ROLE_UUID=$(directus roles create --name admin --admin)
+    # ROLE_UUID=$(directus roles create --name admin --admin)
+    ROLE_UUID=$(node node_modules/directus/dist/cli roles create --name admin --admin)
 
     # Pipe output of above command into this one, adding an initial admin user
-    directus users create --email admin@example.com --password password --role $ROLE_UUID
+    # directus users create --email admin@example.com --password password --role $ROLE_UUID
+    node node_modules/directus/dist/cli' users create --email admin@example.com --password password --role $ROLE_UUID
 
     # Create file that indicates first deploy and installation has been completed.
     touch var/platformsh.installed
