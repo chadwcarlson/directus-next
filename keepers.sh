8cfee2ba369efb0ec02e1bcb7f9aeeb99d009976
chadcarlson <chad.carlson@platform.sh>
2020-10-28 11:05:55 -0400

    keep subdirs for local.

---
+++ keepers.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+touch uploads/.gitkeep
+touch extensions/displays/.gitkeep
+touch extensions/endpoints/.gitkeep
+touch extensions/hooks/.gitkeep
+touch extensions/interfaces/.gitkeep
+touch extensions/layouts/.gitkeep
+touch extensions/modules/.gitkeep
