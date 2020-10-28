6727182a7804366d8e040f5025fedd873e7aa4c5
chadcarlson <chad.carlson@platform.sh>
2020-10-28 12:06:56 -0400

    dev/null.

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-if [ -n "$(ls -A extensions)" ]; then
+if [ -n "$(ls -A extensions  2>/dev/null)" ]; then
     mkdir extensions-temp && mv extensions/* extensions-tmp
 fi        
-if [ -n "$(ls -A uploads)" ]; then
+if [ -n "$(ls -A uploads 2>/dev/null)" ]; then
     mkdir uploads-tmp && mv uploads/* uploads-tmp
 fi
--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-if [ -n "$(ls -A extensions-tmp)" ]; then
+if [ -n "$(ls -A extensions-tmp 2>/dev/null)" ]; then
     cp -r extensions-tmp/* extensions
 fi        
-if [ -n "$(ls -A uploads-tmp)" ]; then
+if [ -n "$(ls -A uploads-tmp 2>/dev/null)" ]; then
     cp -r uploads-tmp/* uploads
 fi
